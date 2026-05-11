e2c2bb6939d540c9b93a434eb56be5e682ae38c2
柏瀬 舞香 <oibc37171@ojs.ac.jp>
2026-05-11 11:30:14 +0900

    2.1.4完成

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //クラス2.1.1
+    public class Song {
+        //タイトル
+        public string Title { get; set; } = string.Empty;
+        //アーティスト名
+        public string ArtistName { get; set; } = string.Empty;
+        //時間
+        public int Length { get; set; }
+
+        //コンストラクタ2.1.2
+        public Song(string title,string artistName,int length) {
+            Title = title;
+            ArtistName = artistName;
+            Length = length;
+        }
+
+
+    }
+}
