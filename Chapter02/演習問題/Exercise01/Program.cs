525206c06af0097ea885d16d46a18281fca2f316
柏瀬 舞香 <oibc37171@ojs.ac.jp>
2026-05-13 10:16:21 +0900

    2.1.4まで完成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -1,23 +1,48 @@
 ﻿
+using System.Reflection;
+
 namespace Exercise01 {
     internal class Program {
         static void Main(string[] args) {
 
             //2-1.3
-            var songs = new Song[] {
-                new Song("Let it be", "The Beatles", 243),
-                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
-                new Song("Close To You", "Carpenters", 276),
-                new Song("Honesty", "Billy Joel", 231),
-                new Song("I Will Always Love You", "Whitney Houston", 273),
-            };
-            PrintSongs(songs);//Alt+Enter
+            var songs = new List<Song>();
+            Console.WriteLine("**** 曲の登録 ****");
+
+            while (true) {
+                Console.Write("曲名:");
+                var title = Console.ReadLine();
+                if (title == "end") {
+                    break;
+                }
+                Console.Write("アーティスト名:");
+                var artistName = Console.ReadLine();
+                Console.Write("演奏時間（秒）:");
+                int length = int.Parse(Console.ReadLine());
+                Song song = new Song(title, artistName, length);
+                Console.WriteLine();
+
+                songs.Add(song);
+            }
+            PrintSongs(songs);
         }
+      
+        
+
+        //    var songs = new Song[] {
+        //        new Song("Let it be", "The Beatles", 243),
+        //        new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
+        //        new Song("Close To You", "Carpenters", 276),
+        //        new Song("Honesty", "Billy Joel", 231),
+        //        new Song("I Will Always Love You", "Whitney Houston", 273),
+        //    };
+        //    PrintSongs(songs);//Alt+Enter
+        //}
 
         //2-1.4
-        private static void PrintSongs(Song[] songs) {
+        private static void PrintSongs(IEnumerable <Song> songs) {
             foreach (var song in songs) {
-                Console.WriteLine($"{song.Title},{song.ArtistName},{song.Length / 60 + ":" + song.Length % 60}");
+                Console.WriteLine($"{song.Title},{song.ArtistName},{song.Length/60}:{(song.Length%60):00}");//ゼロサプレス
             }
         }
     }
---
+++ Chapter02/演習問題/exercise02/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
