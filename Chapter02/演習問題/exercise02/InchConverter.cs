3ddd46a90c794f836ceb6a632c42049d1b3ab527
柏瀬 舞香 <oibc37171@ojs.ac.jp>
2026-05-13 11:51:09 +0900

    コード3.1

--- Chapter02/演習問題/exercise02/InchConverter.cs
+++ Chapter02/演習問題/exercise02/InchConverter.cs
@@ -8,7 +8,12 @@ namespace exercise02 {
     public static class InchConverter {
         public static readonly double ratio = 0.0254;
 
+        //メートルからインチに変換
+        public static double ToInch(double meter) {
+            return meter / ratio;
+        }
 
+        //インチからメートルに換算
         public static double ToMeter(double inch) {
             return inch * ratio;
         }
