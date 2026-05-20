af56da483f59d7926208b6adb2aa2b6986d9c7b0
柏瀬 舞香 <oibc37171@ojs.ac.jp>
2026-05-20 12:04:36 +0900

    問題3.1.1完成

---
+++ Chapter03/演習問題/Exercise01/Program.cs
@@ -0,0 +1,43 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var numbers = new List<int> { 12, 87, 94, 14, 53, 20, 40, 35, 76, 91, 31, 17, 48 };
+
+            // 3.1.1
+            Console.WriteLine("---3.1.1---");
+            Exercise1(numbers);
+            Console.WriteLine("-----");
+            Console.WriteLine();
+
+            // 3.1.2
+            Exercise2(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.3
+            Exercise3(numbers);
+            Console.WriteLine("-----");
+
+            // 3.1.4
+            Exercise4(numbers);
+        }
+
+        private static void Exercise1(List<int> numbers) {
+            var exists = numbers.Exists(T => T % 8 == 0 || T % 9 == 0);
+            if (exists) {
+                Console.WriteLine("存在しています");
+            } else {
+                Console.WriteLine("存在していません");
+            }
+        }
+
+        private static void Exercise2(List<int> numbers) {
+        }
+
+        private static void Exercise3(List<int> numbers) {
+        }
+
+        private static void Exercise4(List<int> numbers) {
+        }
+    }
+}
