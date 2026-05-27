5e05920ca1b02cd06a42b6fb3bb90ab1cc2cb1c3
柏瀬 舞香 <oibc37171@ojs.ac.jp>
2026-05-27 11:31:39 +0900

    問題4.1（3以外）修正

--- Chapter04/演習問題/exercise01/Program.cs
+++ Chapter04/演習問題/exercise01/Program.cs
@@ -17,35 +17,48 @@ namespace exercise01 {
         private static void Exercise1(List<string> langs) {
             //foreach文
             Console.WriteLine("foreach文で出力");
-            var name = langs.FindAll(s => s.Contains('S'));          
-                foreach (var s in name) {
-                    Console.WriteLine(s);                
+            foreach (var lang in langs) {
+                if (lang.Contains('S'))
+                    Console.WriteLine(lang);
             }
+            //var name = langs.FindAll(s => s.Contains('S'));          
+            //    foreach (var s in name) {
+            //        Console.WriteLine(s);
+
 
             //for文
             Console.WriteLine("\nfor文で出力");
-            var cut = langs.Where(s => s.Contains('S')).ToArray();
-            for (int i =0;i<cut.Length;i++) {
-                Console.WriteLine(cut[i]);
+            for (int i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains('S'))
+                    Console.WriteLine(langs[i]);
             }
+            //var cut = langs.Where(s => s.Contains('S')).ToArray();
+            //for (int i =0;i<cut.Length;i++) {
+            //    Console.WriteLine(cut[i]);
+
+
 
             //while文
             Console.WriteLine("\nwhili文で出力");
-            int j = 0;
-            var count = langs.Where(s => s.Contains('S')).ToArray();
-            while (j < count.Length) {
-                Console.WriteLine(count[j]);
-                j++;
+            int count = 0;
+            //var count = langs.Where(s => s.Contains('S')).ToArray();
+            while (count < langs.Count) {
+                if (langs[count].Contains('S'))
+                    Console.WriteLine(langs[count]);
+                count++;
             }
 
         }
 
         private static void Exercise2(List<string> langs) {
-            
+            //LINQを使用する
+            var selected = langs.Where(s => s.Contains('S'));
+            foreach (var lang in selected) {
+                Console.WriteLine(lang);
+            }
         }
 
         private static void Exercise3(List<string> langs) {
-
         }
     }
 }
