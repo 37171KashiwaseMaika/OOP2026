1d5ac9d5f5168e3e5674e9fde4d042134af4feae
柏瀬 舞香 <oibc37171@ojs.ac.jp>
2026-06-01 11:03:28 +0900

    問題5.1.2　完成

---
+++ Chapter05/演習問題/Exercise01/YearMonth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //5.1.1
+    public class YearMonth {
+        public int Year { get; init; }
+        public int Month { get; init; }
+
+        //コンストラクタ
+        public YearMonth(int year, int month) {
+            this.Year = year;
+            this.Month = month;
+        }
+        //5.1.2(P116参照)
+        public bool Is21Century => (2001 <= Year && Year <= 2100);
+
+        //5.1.3
+        public YearMonth AddOneMonth() {
+
+        }
+
+        //5.1.4
+        public override string ToString() =>
+    }
+}
