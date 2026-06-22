c5507c4807380bc62150a1cf86d1cd141effef0f
柏瀬 舞香 <oibc37171@ojs.ac.jp>
2026-06-22 11:30:45 +0900

    設問１_Studentクラス完成

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -1,7 +1,9 @@
 ﻿namespace Test01_01 {
     public class Student {
-        
 
 
+        private string Name { get; set; } = string.Empty;       
+        private string Subject { get; set; } = string.Empty;       
+        private int Score { get; set; }
     }
 }
