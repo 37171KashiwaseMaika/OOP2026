1f7f7555fbe537df1f0bb77836629e6481fcf88c
柏瀬 舞香 <oibc37171@ojs.ac.jp>
2026-06-22 12:04:50 +0900

    設問２_提出

--- TestProject/Test01_02/Program.cs
+++ TestProject/Test01_02/Program.cs
@@ -61,34 +61,42 @@
         //問題１　合計値を表示（式形式で記述せよ）
         //　　　　出力結果【618】
         private static void Exercise01(int[] numbers) {
-
+            var sum = numbers.Sum();
+            Console.WriteLine(sum);
         }
 
 
         //問題２　偶数の最大値を表示（式形式で記述せよ）
         //　　　　出力結果【94】
         private static void Exercise02(int[] numbers) {
-
+            var max = numbers.Where(n => n % 2 == 0).Max();
+            Console.WriteLine(max);
         }
 
 
         //問題３　昇順に並べて表示（遅延実行とする）
         //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
         private static void Exercise03(int[] numbers) {
-            
+            var sorted = numbers.OrderBy(n => n);
+            foreach (var item in sorted) {
+                Console.WriteLine(item);
+            }
         }
 
         //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
         //　　　　出力結果【12 14 20 40 35 31 17 48】
         private static void Exercise04(int[] numbers) {
-            
+            var results = numbers.Where(n => n >= 10 && n <= 50);
+            foreach (var item in results) {
+                Console.WriteLine(item);
+            }
         }
 
         //問題５　Countメソッドを使い、小文字の'n'が含まれている都市名がいくつあるかカウントして結果を表示
         //　　　　出力結果【5】
         private static void Exercise05(List<string> cities) {
-      
-        
+            var count = cities.Count(s => s.Contains('n'));
+            Console.WriteLine(count);
         }
 
         //問題６　全都市数
