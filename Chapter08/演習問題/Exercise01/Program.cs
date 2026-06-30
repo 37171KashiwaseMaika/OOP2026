a10dd75f0e9784d323146c68238d39826d2a7cb3
柏瀬 舞香 <oibc37171@ojs.ac.jp>
2026-06-30 15:22:10 +0900

    問題8.1 答え

--- Chapter08/演習問題/Exercise01/Program.cs
+++ Chapter08/演習問題/Exercise01/Program.cs
@@ -11,28 +11,50 @@ namespace Exercise01 {
             Console.WriteLine();
             Exercise2(text);
         }
-        
+
         private static void Exercise1(string text) {
-            var dict = new Dictionary<char,int>();//dict　辞書
-
-
-            var words = text.Split(' ');
-            var sb = new StringBuilder();
-
-            
-            foreach (var item in words.Skip(1)) {
-                var uppt = text.ToUpper();
+            var dict = new Dictionary<char, int>();//dict　辞書
+            foreach (var ch in text.ToUpper()) {
+                if ('A' <= ch && ch <= 'Z') {
+                    if (dict.ContainsKey(ch))
+                        dict[ch]++;
+                    else
+                        dict[ch] = 1;
+                }
+            }
+            foreach (var item in dict.OrderBy(x => x.Key)) {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
             }
 
-            foreach (var item in dict) {
-                //if ('A' <= ch && ch <= 'Z') {
-                //    var counts = item.Count(s => s == ch);
-                //}              
-            }
+
+            //var words = text.Split(' ');
+            //var sb = new StringBuilder();
+
+
+            //foreach (var item in words.Skip(1)) {
+            //    var uppt = text.ToUpper();
+            //}
+
+            //foreach (var item in dict) {
+            //    //if ('A' <= ch && ch <= 'Z') {
+            //    //    var counts = item.Count(s => s == ch);
+            //    //}              
+            //}
         }
 
         private static void Exercise2(string text) {
-            
+            var dict = new SortedDictionary<char, int>();//dict　辞書
+            foreach (var ch in text.ToUpper()) {
+                if ('A' <= ch && ch <= 'Z') {
+                    if (dict.ContainsKey(ch))
+                        dict[ch]++;
+                    else
+                        dict[ch] = 1;
+                }
+            }
+            foreach (var item in dict) {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value);
+            }
         }
     }
 }
