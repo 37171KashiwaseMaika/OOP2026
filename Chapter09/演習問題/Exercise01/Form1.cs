f3390365bb9ef80cc67cfcbd29be4cc2afa0d179
柏瀬 舞香 <oibc37171@ojs.ac.jp>
2026-07-09 15:05:38 +0900

    問題9-1　完成

--- Chapter09/演習問題/Exercise01/Form1.cs
+++ Chapter09/演習問題/Exercise01/Form1.cs
@@ -1,11 +1,28 @@
+using System;
+using System.Globalization;
+
 namespace Exercise01 {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
         }
 
+        //P200
         private void btButton1_Click(object sender, EventArgs e) {
             var dateTime = DateTime.Now;
+            tbOut1.Text = dateTime.ToString("g");
+        }
+
+        private void btButton2_Click(object sender, EventArgs e) {
+            var dateTime = DateTime.Now;
+            tbOut2.Text = dateTime.ToString("yyyy”NMMŒdd“ú@HHmm•ªss•b");
+        }
+
+        private void btButton3_Click(object sender, EventArgs e) {
+            var dateTime = DateTime.Now;
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            tbOut3.Text = dateTime.ToString(" gg y”NMŒd“ú (dddd)",culture);
         }
     }
 }
